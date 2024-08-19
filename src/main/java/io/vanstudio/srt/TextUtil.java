d4b34a7527eb28fe624f2414b1eb992cb0fb8df2
Vanco <van.fan@outlook.com>
2024-08-19 19:27:21 +0800

    normalize text before translate.

--- src/main/java/io/vanstudio/srt/TextUtil.java
+++ src/main/java/io/vanstudio/srt/TextUtil.java
@@ -7,9 +7,10 @@ public class TextUtil {
 
     public static String normalize(String sub) {
         StringBuilder sb = new StringBuilder();
-        String[] split = sub.split(LINE_BREAK);
+        String striped = strip(sub);
+        String[] split = striped.split(LINE_BREAK);
         for (String line : split) {
-            if (line.trim().startsWith(CONV_START)) {
+            if (striped.startsWith(CONV_START)) {
                 if (!sb.isEmpty()) sb.append(LINE_BREAK);
                 sb.append(line);
             } else {
@@ -19,5 +20,17 @@ public class TextUtil {
         }
 
         return sb.toString();
+    }
+
+    private static String strip(String sub) {
+        return stripObj(stripXml(sub.trim()));
+    }
+
+    private static String stripXml(String xml) {
+        return xml.replaceAll("<[^>]+>", "");
+    }
+
+    private static String stripObj(String xml) {
+        return xml.replaceAll("\\{[^}]+}", "");
     }
 }
