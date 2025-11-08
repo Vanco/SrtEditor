c5b880fd365c9cbbc1bbe68b9916a582f896a3ed
Vanco <van.fan@outlook.com>
2025-11-08 10:22:29 +0800

    fix module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,14 +5,20 @@ module io.vanstudio.srt {
     requires javafx.graphics;
     requires jdk.charsets;
     requires com.azure.ai.translation.text;
+    requires context.propagation;
+    requires reactor.blockhound;
     requires com.azure.core;
     requires com.azure.http.netty;
     requires com.azure.json;
     requires com.azure.xml;
-    requires io.netty.transport;
+    requires io.netty.common;
     requires io.netty.handler;
     requires io.netty.handler.proxy;
+    requires io.netty.buffer;
+    requires io.netty.codec;
     requires io.netty.codec.http;
+    requires io.netty.codec.http2;
+    requires io.netty.tcnative.classes.openssl;
 
     opens io.vanstudio.srt to javafx.fxml;
     exports io.vanstudio.srt;
